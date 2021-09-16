c42b61251100fd2a29b72490d83d46fd26cbfcfd
Victoria <vilkova_viktoria2000@mail.ru>
2021-09-16 15:46:46 +0500

    2

---
+++ app/src/main/java/net/intervyaz/viewintro/Music.kt
@@ -0,0 +1,41 @@
+package net.intervyaz.viewintro
+
+import android.app.Service
+import android.content.ContentValues
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Handler
+import android.os.IBinder
+import android.util.Log
+import net.intervyaz.viewintro.R
+import java.util.*
+
+class Music : Service() {
+
+    // Музыка в фоне
+    private var mediaPlayer: MediaPlayer? = null
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer.create(this, R.raw.music_back)
+        mediaPlayer?.isLooping
+        mediaPlayer?.start()
+    }
+
+    // Команда "старт"
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    // Команда "стоп"
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.stop()
+
+    }
+}
--- app/src/main/java/net/intervyaz/viewintro/fragments/SettingsFragment.kt
+++ app/src/main/java/net/intervyaz/viewintro/fragments/SettingsFragment.kt
@@ -1,22 +1,63 @@
 package net.intervyaz.viewintro.fragments
 
+import android.app.Service
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import net.intervyaz.viewintro.R
 
-class SettingsFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+import net.intervyaz.viewintro.R
+import net.intervyaz.viewintro.databinding.FragmentSettingsBinding
+import android.widget.Toast
+import android.widget.CompoundButton
+import net.intervyaz.viewintro.MainActivity
+import net.intervyaz.viewintro.Music
+
+class SettingsFragment : Fragment(R.layout.fragment_settings){
+    private lateinit var binding: FragmentSettingsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Обработка нажатия включения музыки
+        binding.switchMusic.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                //context?.startService(Intent(context, Music::class.java))
+                Toast.makeText(context, "Музыка включена", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                //context?.stopService(Intent(context, Music::class.java))
+                Toast.makeText(context, "Музыка выключена", Toast.LENGTH_SHORT).show()
+            }
+        });
+        // Обработка нажатия изменения текста
+        binding.switchText.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                Toast.makeText(context, "Стандартный шрифт изменён", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(context, "Стандартный шрифт", Toast.LENGTH_SHORT).show()
+            }
+        });
+
+        // Обработка включения темы
+        binding.switchBackground.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                Toast.makeText(context, "Включена тёмная тема", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(context, "Светлая тема", Toast.LENGTH_SHORT).show()
+            }
+        });
     }
 }
